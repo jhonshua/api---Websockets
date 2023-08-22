64ca39000f5dc7af06a1dea186f62a32ae87aeee
Julio Cesar Llinas <109869139+jhonshua@users.noreply.github.com>
2023-08-21 23:05:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
   credentials: true,
-  origin: "https://front-websockets.vercel.app",
+  origin: "https://front-websockets-8s2i.vercel.app",
 }))
 
 //routes
